da61e0165f4d353114622701173b4fd42f3f39cc
Ethan Moss <ethan@Ethans-MacBook-Pro.local>
2025-11-28 08:59:09 +0800

    init.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: "./src/main.ts",
-      fileName: "web-design",
+      entry: "./src/index.ts",
+      name: "web-design",
+      fileName: "index",
     },
   },
+  plugins: [dts()],
 });
